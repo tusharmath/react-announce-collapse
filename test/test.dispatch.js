e596c482c32cd8cb17e15b1929749ec6f68683db
Tushar Mathur <tusharmath@gmail.com>
2016-02-18 19:46:06 +0530

    refactor(testDispatch): remove unused testSubscriber

--- test/test.dispatch.js
+++ test/test.dispatch.js
@@ -4,12 +4,6 @@ import d from '../src/dispatch'
 
 const {onNext} = ReactiveTest
 
-const testObserver = s => {
-  const out = []
-  s.subscribe(x => out.push(x))
-  return out
-}
-
 test('dispose', t => {
   const sh = new TestScheduler()
   const state = sh.createHotObservable()
