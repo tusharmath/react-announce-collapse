784dcc22a24a336d11030a6fd9f041293ac6b9ae
Tushar Mathur <tusharmath@gmail.com>
2016-02-18 17:47:46 +0530

    test(dispatch): add unit tests
    event:COLLAPSE, event:distinct

--- src/dispatch.js
+++ src/dispatch.js
@@ -2,6 +2,7 @@ const targs = require('argtoob')
 
 module.exports = (state, component) => {
   return state
+    .distinctUntilChanged()
     .withLatestFrom(component, targs('state', 'component'))
     .subscribe(x => x.component.dispatch('COLLAPSE', x.state))
 }
--- test/test.dispatch.js
+++ test/test.dispatch.js
@@ -16,3 +16,32 @@ test('dispose', t => {
   const component = sh.createHotObservable()
   t.true(Disposable.isDisposable(d(state, component)))
 })
+
+test('event:COLLAPSE', t => {
+  const out = []
+  const _component = {dispatch: (ev, state) => out.push({ev, state})}
+  const sh = new TestScheduler()
+  const state = sh.createHotObservable(onNext(210, true), onNext(220, false))
+  const component = sh.createHotObservable(onNext(200, _component))
+  d(state, component)
+  sh.start()
+  t.same(out, [
+    {ev: 'COLLAPSE', state: true},
+    {ev: 'COLLAPSE', state: false}
+  ])
+})
+
+test('event:distinct', t => {
+  const out = []
+  const _component = {dispatch: (ev, state) => out.push(state)}
+  const sh = new TestScheduler()
+  const state = sh.createHotObservable(
+    onNext(210, true),
+    onNext(220, false),
+    onNext(230, false)
+  )
+  const component = sh.createHotObservable(onNext(200, _component))
+  d(state, component)
+  sh.start()
+  t.same(out, [true, false])
+})
