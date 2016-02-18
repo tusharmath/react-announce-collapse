0cff244eb36e44de309804b92e52d15004b5125a
Tushar Mathur <tusharmath@gmail.com>
2016-02-18 17:12:56 +0530

    test(getCollapsable): add target:inside:skipped test

--- src/getCollapsable.js
+++ src/getCollapsable.js
@@ -13,5 +13,5 @@ module.exports = (esc, target, node, state, hasParent, skip) => Rx
         .combineLatest(node, targs('target', 'node'))
         .filter(x => hasParent(x.target, x.node))
         .withLatestFrom(state, (a, b) => b)
-        .map(x => !Boolean(x))
+        .map(x => skip ? x : !Boolean(x))
 )
