6949eaee31a534feec40b724499fd813f4c1a86d
Tushar Mathur <tusharmath@gmail.com>
2016-02-29 20:18:01 +0530

    feat(utils): add isActive() method
    isActive simply filters the state stream and resolves to a boolean value

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const ReactDOM = require('react-dom')
 const e = require('./src/collapsable')
 
 exports.collapsable = e(e, ReactDOM, window)
+exports.isActive = src => src
+  .filter(x => x.event === 'COLLAPSE')
+  .map(x => x.args[0])
+  .distinctUntilChanged()
